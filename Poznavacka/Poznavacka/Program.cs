9eca725dfb5438f6d86b540ff394607f77c5c5b1
Vojtěch Pekárek <panpegisek@gmail.com>
2025-12-18 19:16:27 +0100

    Initial commit

---
+++ Poznavacka/Poznavacka/Program.cs
@@ -0,0 +1,61 @@
+﻿
+using Microsoft.VisualBasic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> Items = GetItems("Data");
+        Zkouseni(Items);
+    }
+
+    public static List<String> GetItems(string path)
+    {
+        List<String> Data = new List<String>();
+        var directories = Directory.GetDirectories(path);
+        foreach (string d in directories)
+        {
+            Data.AddRange(GetItems(d));
+        }
+
+        string[] contents = Directory.GetFiles(path);
+        List<string> tmp = new List<string>();
+
+        foreach (string c in contents)
+        {
+            Data.Add(c);
+        }
+        return Data;
+    }
+
+    public static void Zkouseni(List<string> Items)
+    {
+        Console.WriteLine("\n");
+
+        Console.ReadLine();
+        Random.Shared.Shuffle(CollectionsMarshal.AsSpan(Items));
+
+        foreach (string c in Items)
+        {
+            string path = Path.GetDirectoryName(c) + "\\x.jpg";
+            File.Copy(c, path, true);
+
+            var p = new Process();
+            p.StartInfo = new ProcessStartInfo(path)
+            {
+                UseShellExecute = true
+            };
+            p.Start();
+
+            Console.ReadLine();
+            Console.WriteLine(c);
+            Console.ReadLine();
+
+            File.Delete(path);
+        }
+    }
+}
